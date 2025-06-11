6d388a294216864717fca41589cdd1be0e23e226
Erik-rosol <erik.vila@rosol-technology.com>
2025-06-11 14:19:26 +0200

    Last exercises and test push

--- src/test/java/TaskManagerTest.java
+++ src/test/java/TaskManagerTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TaskManagerTest {
+
+    @Test
+    public void testAddTaskToManager(){
+    }
+    @Test
+    public void testCompleteTaskManager(){
+    }
+    @Test
+    public void testRemoveTask(){
+    }
+
 }
